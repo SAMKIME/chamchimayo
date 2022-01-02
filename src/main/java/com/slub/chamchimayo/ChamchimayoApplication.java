c29938eaaa5aba807ab5ebd88565cb2087f1acd6
김민섭 <33247303+meeansub@users.noreply.github.com>
2022-01-02 22:37:43 +0900

    project init

---
+++ src/main/java/com/slub/chamchimayo/ChamchimayoApplication.java
@@ -0,0 +1,13 @@
+package com.slub.chamchimayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamchimayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamchimayoApplication.class, args);
+	}
+
+}
